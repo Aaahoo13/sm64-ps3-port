50c186a88e9f3dd7ccef2d28ab644e227e514247
Aaahoo13 <sharaman13@hotmail.fr>
2026-01-17 17:52:28 +0100

    Update gfx_ps3_rapi.c
    Deletion of "rsxSetZControl(rsx_ctx, 1, 1, 0)".

--- src/pc/gfx/gfx_ps3_rapi.c
+++ src/pc/gfx/gfx_ps3_rapi.c
@@ -447,7 +447,6 @@ static inline void draw_set_environment(void) {
     rsxSetColorMaskMrt(rsx_ctx, 0);
     rsxSetClearColor(rsx_ctx, 0);
     rsxSetClearDepthStencil(rsx_ctx, 0xffffff00);
-    rsxSetZControl(rsx_ctx, 1, 1, 0);
     rsxSetUserClipPlaneControl(
         rsx_ctx,
         GCM_USER_CLIP_PLANE_DISABLE,
